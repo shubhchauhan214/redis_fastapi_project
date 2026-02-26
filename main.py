6daf35a051f08017558f4d1c9ddccb84768bdbb9
Shubham Chauhan <shubhchauhan616@gmail.com>
2026-02-26 20:08:47 +0530

    Added Redis caching to get students api

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from sqlalchemy.orm import Session
 from database import engine, get_db
 import models
 from schemas import StudentCreate
+from redis_client import redis_client
+import json
 
 app = FastAPI()
 
@@ -16,6 +18,8 @@ def create_student(student: StudentCreate, db: Session=Depends(get_db)):
     db.commit()
     db.refresh(new_student)
 
+    redis_client.delete("students_list")
+
     return{
         "message":"Student Created Successfully",
         "student_id": new_student.id
@@ -24,17 +28,30 @@ def create_student(student: StudentCreate, db: Session=Depends(get_db)):
 @app.get("/students")
 def get_students(db: Session = Depends(get_db)):
 
-    students = db.query(models.Student).all()
+    #Check Cache
+    cached_data = redis_client.get("students_list")
+
+    if cached_data:
+        print("From Redis")
+        return json.loads(cached_data)
+    
+    print("From Database")
+
+    # Fetch from DB
+    students=db.query(models.Student).all()
 
     result = [
         {
             "id": s.id,
-            "name": s.name,
-            "course": s.course,
-            "city": s.city
+            "name":s.name,
+            "course":s.course,
+            "city":s.city
         }
         for s in students
     ]
+
+    #Store in Redis
+    redis_client.set("students_list", json.dumps(result), ex=60)
 
     return result
 
